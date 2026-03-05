df648f9cd71e2328dc9e3f840205fbaf23b7c500
slideglide <68702247+slideglide@users.noreply.github.com>
2026-03-05 22:57:29 +0300

    Geode v5

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,21 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
-
-#include <geode.custom-keybinds/include/OptionalAPI.hpp>
-
 #include "Profiler.hpp"
-#include "platform/IPlatform.hpp"
-
 #include <fxprof/Profile.hpp>
 
 using namespace geode::prelude;
 
 $on_mod(Loaded) {
-    // platform::init();
-
-    using namespace keybinds;
-
-    (void)[]() -> Result<> {
-        GEODE_UNWRAP(BindManagerV2::registerBindable(GEODE_UNWRAP(BindableActionV2::create(
-            "toggle_profiler"_spr,
-            "Toggle Profiler",
-            "Starts or stops the profiler.",
-            { GEODE_UNWRAP(KeybindV2::create(KEY_F6, ModifierV2::Control)) },
-            GEODE_UNWRAP(CategoryV2::create("Profiler")),
-            false
-        ))));
-        return Ok();
-    }();
-
-    new EventListener(+[](InvokeBindEventV2* event) {
-        if (!event->isDown()) {
+    listenForKeybindSettingPresses("toggle-profiler", [](Keybind const& keybind, bool down, bool repeat, double timestamp) {
+        if (down && !repeat) {
             auto& profiler = Profiler::get();
             if (profiler.isRunning()) {
                 profiler.stop();
                 log::info("Profiler stopped.");
-            } else {
+            }
+            else {
                 profiler.start();
                 log::info("Profiler started.");
             }
         }
-        return ListenerResult::Propagate;
-    }, InvokeBindFilterV2(nullptr, "toggle_profiler"_spr));
-}
+    });
+};
